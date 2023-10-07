c914923dc8bf2aeb96ec6b0f4eda2adfa36c6777
AngelloAbor <angello.bor@gmail.com>
2023-10-06 23:50:00 -0300

    first commit

---
+++ BD-Colaboradores/src/components/Listado.jsx
@@ -0,0 +1,35 @@
+const Listado = ({colaboradores,enviarId}) => {
+    
+    return (
+        <>
+            <table className="table">
+                <thead>
+                    <tr>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Correo</th>
+                    <th scope="col">Edad</th>
+                    <th scope="col">Cargo</th>
+                    <th scope="col">Telefono</th>
+                    <th scope="col">Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {colaboradores.map(colaborador =>
+                        <tr key={colaborador.id}>
+                            <td>{colaborador.nombre}</td>
+                            <td>{colaborador.correo}</td>
+                            <td>{colaborador.edad}</td>
+                            <td>{colaborador.cargo}</td>
+                            <td>{colaborador.telefono}</td>
+                            <td className="text-center">
+                                <button className="btn btn-light" onClick={()=> {enviarId(colaborador.id)}}>🗑️</button>
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default Listado
